b0905ff45f82d1829c1229988f04225429629b0b
Anish Gupta <anishprogrammer007@gmail.com>
2025-11-12 23:08:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bookstore-1-tnqb.onrender.com",
     credentials: true,
   })
 );
