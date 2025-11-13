9d69cbfb5dbc06e61d2ae4b113e3a4af33e7e0e0
Anish Gupta <anishprogrammer007@gmail.com>
2025-11-13 20:01:26 +0530

    Fix import path for Dashboard.css

--- client/src/Components/Deshbord.jsx
+++ client/src/Components/Deshbord.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../css/dashboard.css";
+import "../css/Dashboard.css";
 import axios from "axios";
 const Deshbord = () => {
   const [student, setStudent] = useState(0);
